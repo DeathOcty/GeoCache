8da3f0aed608a28365e6130acc58aa4259e6da3c
notslawkid <JBwalston@student.fullsail.edu>
2018-09-05 14:31:58 -0400

    Added some stuff.

---
+++ MyNP/MyNP.ino
@@ -0,0 +1,33 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       MyNP.ino
+    Created:	9/5/2018 10:06:37 AM
+    Author:     DESKTOP-EPFUER1\notsl
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+#include <SoftwareSerial.h>
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
